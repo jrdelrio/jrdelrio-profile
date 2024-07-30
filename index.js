2d0a995566a6102409b6c59871880d1618ae4378
jrdelrio <jrdelriodom@gmail.com>
2024-07-30 14:30:09 +0200

    arreglo del form

--- index.js
+++ index.js
@@ -24,18 +24,21 @@ function closeMenu() {
 
 const scriptURL =
   "https://script.google.com/macros/s/AKfycby3XbC_fQotPUpeDLVL-GOC3H_83ye20eeNYybnyrr4FB88_-uQoDKW-2iYZnrTb3mn/exec";
-  const form = document.forms['submit-to-google-sheet']
-  const msg = document.getElementById("msg")
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
 
-  form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => {
-          msg.innerHTML = "Message sent successfully"
-          setTimeout(function(){
-              msg.innerHTML = ""
-          },5000)
-          form.reset()
-      })
-      .catch(error => console.error('Error!', error.message))
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, {
+    method: "POST",
+    body: new FormData(form),
   })
+    .then((response) => {
+      msg.innerHTML = "Message sent successfully";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 5000);
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
